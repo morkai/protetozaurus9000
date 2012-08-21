df8847e78a0c7db897f64bd62d32c8d56143d755
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2012-08-21 16:55:18 +0200

    Updated the db structure to the latest version.

--- structure.sql
+++ structure.sql
@@ -1,6 +1,7 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+DROP TABLE IF EXISTS `colors`;
 CREATE TABLE IF NOT EXISTS `colors` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_polish_ci NOT NULL,
@@ -8,6 +9,7 @@ CREATE TABLE IF NOT EXISTS `colors` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=25 ;
 
+DROP TABLE IF EXISTS `contacts`;
 CREATE TABLE IF NOT EXISTS `contacts` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `company` varchar(100) COLLATE utf8_polish_ci NOT NULL,
@@ -23,6 +25,15 @@ CREATE TABLE IF NOT EXISTS `contacts` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=4 ;
 
+DROP TABLE IF EXISTS `quantities`;
+CREATE TABLE IF NOT EXISTS `quantities` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=43 ;
+
+DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nr` varchar(50) COLLATE utf8_polish_ci NOT NULL,
@@ -32,10 +43,9 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   `patient` int(10) unsigned DEFAULT NULL,
   `quantity` decimal(7,2) unsigned NOT NULL DEFAULT '0.00',
   `unit` varchar(20) COLLATE utf8_polish_ci NOT NULL DEFAULT 'szt.',
+  `price` decimal(8,2) unsigned NOT NULL DEFAULT '0.00',
   `color` int(10) unsigned DEFAULT NULL,
   `worktype` int(10) unsigned DEFAULT NULL,
-  `metal` tinyint(1) NOT NULL DEFAULT '0',
-  `zircon` tinyint(1) NOT NULL DEFAULT '0',
   `notes` text COLLATE utf8_polish_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nr` (`nr`),
@@ -43,14 +53,25 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   KEY `color` (`color`),
   KEY `worktype` (`worktype`),
   KEY `doctor` (`doctor`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=8 ;
 
+DROP TABLE IF EXISTS `worktypes`;
 CREATE TABLE IF NOT EXISTS `worktypes` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  `price` decimal(8,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=60 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=61 ;
+
+DROP TABLE IF EXISTS `worktype_prices`;
+CREATE TABLE IF NOT EXISTS `worktype_prices` (
+  `doctor` int(10) unsigned NOT NULL,
+  `worktype` int(10) unsigned NOT NULL,
+  `price` decimal(8,2) unsigned NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`doctor`,`worktype`),
+  KEY `worktype` (`worktype`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 
 ALTER TABLE `tasks`
@@ -58,3 +79,7 @@ ALTER TABLE `tasks`
   ADD CONSTRAINT `tasks_ibfk_4` FOREIGN KEY (`color`) REFERENCES `colors` (`id`),
   ADD CONSTRAINT `tasks_ibfk_5` FOREIGN KEY (`worktype`) REFERENCES `worktypes` (`id`),
   ADD CONSTRAINT `tasks_ibfk_6` FOREIGN KEY (`doctor`) REFERENCES `contacts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `worktype_prices`
+  ADD CONSTRAINT `worktype_prices_ibfk_2` FOREIGN KEY (`worktype`) REFERENCES `contacts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `worktype_prices_ibfk_1` FOREIGN KEY (`doctor`) REFERENCES `contacts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
